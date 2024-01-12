1297dbc2108bc1d527ccad07096c321f93241a22
Enrico Carteciano <carteciano.enrico@gmail.com>
2024-01-12 20:09:00 +0100

    update

--- src/pages/fahrzeuge.js
+++ src/pages/fahrzeuge.js
@@ -128,7 +128,7 @@ function CarListingLeftSidebar() {
                 <div className="col-lg-12">
                   <div className="show-item-and-filter">
                     <p>
-                      Es gibt <strong>{filteredCars.length}</strong> verfügbare
+                      Es gibt HALLOOO <strong>{filteredCars.length}</strong> verfügbare
                       Autos.
                     </p>
                   </div>
